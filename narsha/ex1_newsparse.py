0babcecaec65f67c978e62f309d64a7dfbbd663b
cireisen <kdh_214@naver.com>
2019-04-01 20:30:06 +0900

    신문 크롤링 예제

--- narsha/ex1_newsparse.py
+++ narsha/ex1_newsparse.py
@@ -1,1 +1,14 @@
-printf("Hello, World!")
+#pip install newspaper3
+from newspaper import Article
+
+#크롤링 할 뉴스 주소
+url = 'http://v.media.daum.net/v/20170604205121164'
+
+#언어 한국어
+lang = Article(url, language = 'ko')
+lang.download()
+lang.parse()
+
+print(lang.title)
+
+print(lang.text[:150])
