1098835f2b917e7911836c23d03db8d7c87e3850
DooSunnny <0308engo@gmail.com>
2019-04-09 12:11:37 +0900

    call_stt_tts

--- narsha/ex1_call_stt_tts.py
+++ narsha/ex1_call_stt_tts.py
@@ -125,3 +125,20 @@ def getText2VoiceStream(inText,inFileName):
 			writeFile.write(response.audioContent)
 	writeFile.close()
 	return response.resOptions.resultCd
+
+def main():
+	output_file = "testtts.wav"
+	
+	test_return=cts.test()
+	
+	if(int(test_return)==200):
+		text = cts.getVoice2Text()
+		print("text : %s"%text)
+
+	if(text):
+		getText2VoiceStream("안녕하세요. 반갑습니다.",output_file)
+		play_file(output_file)
+
+	text=False
+if __name__ == '__main__':
+	main()
