5284e8b9547688b1a4c30beedc203d9c1af09e28
DooSunnny <0308engo@gmail.com>
2019-04-09 11:17:09 +0900

    호출후 stt & tts

--- narsha/ex1_call_stt_tts.py
+++ narsha/ex1_call_stt_tts.py
@@ -5,75 +5,28 @@
 
 from __future__ import print_function
 
-import grpc
-
-import gigagenieRPC_pb2
-import gigagenieRPC_pb2_grpc
-
-import os
-import datetime
-import hmac
-import hashlib
-
-### STT
-import pyaudio
 import audioop
-from six.moves import queue
 from ctypes import *
 import RPi.GPIO as GPIO
 import ktkws # KWS
+import MicrophoneStream as MS
+import user_auth as UA
+import os
+
+HOST = 'gate.gigagenie.ai'
+PORT = 4080
+RATE = 16000
+CHUNK = 512
+
 KWSID = ['기가지니', '지니야', '친구야', '자기야']
+RATE = 16000
+CHUNK = 512
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(31, GPIO.OUT)
 btn_status = False
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-CHUNK = 512
-
-# Config for GiGA Genie gRPC
-CLIENT_ID = 'Y2xpZW50X2lkMTU1MjEyNTM2MDY5Ng=='
-CLIENT_KEY = 'Y2xpZW50X2tleTE1NTIxMjUzNjA2OTY='
-CLIENT_SECRET = 'Y2xpZW50X3NlY3JldDE1NTIxMjUzNjA2OTY='
-HOST = 'gate.gigagenie.ai'
-PORT = 4080
-
-
-def getMetadata():
-    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[:-3]
-    message = CLIENT_ID + ':' + timestamp
-
-    signature = hmac.new(CLIENT_SECRET.encode(), message.encode(), hashlib.sha256).hexdigest()
-
-    metadata = [('x-auth-clientkey', CLIENT_KEY),
-                ('x-auth-timestamp', timestamp),
-                ('x-auth-signature', signature)]
-
-    return metadata
-
-def credentials(context, callback):
-    callback(getMetadata(), None)
-
-def getCredentials():
-    sslCred = grpc.ssl_channel_credentials()
-
-    authCred = grpc.metadata_call_credentials(credentials)
-
-    return grpc.composite_channel_credentials(sslCred, authCred)
-
-### END OF COMMON ###
-
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-def py_error_handler(filename, line, function, err, fmt):
-  dummy_var = 0
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so')
-asound.snd_lib_error_set_handler(c_error_handler)
-
 
 def callback(channel):  
 	print("falling edge detected from pin {}".format(channel))
@@ -84,121 +37,27 @@ def callback(channel):
 GPIO.add_event_detect(29, GPIO.FALLING, callback=callback, bouncetime=10)
 
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
 def py_error_handler(filename, line, function, err, fmt):
   dummy_var = 0
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 asound = cdll.LoadLibrary('libasound.so')
 asound.snd_lib_error_set_handler(c_error_handler)
 
-
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-CHUNK = 512
-
-
-
-# MicrophoneStream - original code in https://goo.gl/7Xy3TT
-class MicrophoneStream(object):
-	"""Opens a recording stream as a generator yielding the audio chunks."""
-	def __init__(self, rate, chunk):
-		self._rate = rate
-		self._chunk = chunk
-
-		# Create a thread-safe buffer of audio data
-		self._buff = queue.Queue()
-		self.closed = True
-
-	def __enter__(self):
-		self._audio_interface = pyaudio.PyAudio()
-		self._audio_stream = self._audio_interface.open(
-			format=pyaudio.paInt16,
-			channels=1, rate=self._rate,
-			input=True, frames_per_buffer=self._chunk,
-			# Run the audio stream asynchronously to fill the buffer object.
-			# This is necessary so that the input device's buffer doesn't
-			# overflow while the calling thread makes network requests, etc.
-			stream_callback=self._fill_buffer,
-		)
-
-		self.closed = False
-
-		return self
-
-	def __exit__(self, type, value, traceback):
-		self._audio_stream.stop_stream()
-		self._audio_stream.close()
-		self.closed = True
-		# Signal the generator to terminate so that the client's
-		# streaming_recognize method will not block the process termination.
-		self._buff.put(None)
-		self._audio_interface.terminate()
-
-	def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-		"""Continuously collect data from the audio stream, into the buffer."""
-		self._buff.put(in_data)
-		return None, pyaudio.paContinue
-
-	def generator(self):
-		while not self.closed:
-			# Use a blocking get() to ensure there's at least one chunk of
-			# data, and stop iteration if the chunk is None, indicating the
-			# end of the audio stream.
-			chunk = self._buff.get()
-			if chunk is None:
-				return
-			data = [chunk]
-
-			# Now consume whatever other data's still buffered.
-			while True:
-				try:
-					chunk = self._buff.get(block=False)
-					if chunk is None:
-						return
-					data.append(chunk)
-				except queue.Empty:
-					break
-
-			yield b''.join(data)
-# [END audio_stream]
-
-def print_rms(rms):
-	out = ''
-	for _ in range(int(round(rms/30))):
-		out = out + '*'
-	
-	#print (out)
-
-import wave
-
-def play_file(fname):
-	# create an audio object
-	wf = wave.open(fname, 'rb')
-	p = pyaudio.PyAudio()
-	chunk = 1024
-
-	# open stream based on the wave object which has been input.
-	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-					channels=wf.getnchannels(),
-					rate=wf.getframerate(),
-					output=True)
-
-	# read data (based on the chunk size)
-	data = wf.readframes(chunk)
-
-	# play stream (looping from beginning of file to the end)
-	while len(data) > 0:
-		# writing to the stream is what *actually* plays the sound.
-		stream.write(data)
-		data = wf.readframes(chunk)
-
-		# cleanup stuff.
-	stream.close()
-	p.terminate()
+def generate_request():
+    with MS.MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+    
+        for content in audio_generator:
+            message = gigagenieRPC_pb2.reqVoice()
+            message.audioContent = content
+            yield message
+            
+            rms = audioop.rms(content,2)
+            #print_rms(rms)
 
 def detect():
-	with MicrophoneStream(RATE, CHUNK) as stream:
+	with MS.MicrophoneStream(RATE, CHUNK) as stream:
 		audio_generator = stream.generator()
 
 		for content in audio_generator:
@@ -208,21 +67,8 @@ def detect():
 			#print('audio rms = %d' % (rms))
 
 			if (rc == 1):
-				play_file("../data/sample_sound.wav")
+				MS.play_file("../data/sample_sound.wav")
 				return 200
-
-def generate_request():
-
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-    
-        for content in audio_generator:
-            message = gigagenieRPC_pb2.reqVoice()
-            message.audioContent = content
-            yield message
-            
-            rms = audioop.rms(content,2)
-            print_rms(rms)
 
 def test(key_word = '기가지니'):
 	rc = ktkws.init("../data/kwsmodel.pack")
@@ -239,8 +85,7 @@ def test(key_word = '기가지니'):
 
 def getVoice2Text():	
     print ("\n\n음성인식을 시작합니다.\n\n종료하시려면 Ctrl+\ 키를 누루세요.\n\n\n")
-	
-    channel = grpc.secure_channel('{}:{}'.format(HOST, PORT), getCredentials())
+    channel = grpc.secure_channel('{}:{}'.format(HOST, PORT), UA.getCredentials())
     stub = gigagenieRPC_pb2_grpc.GigagenieStub(channel)
     request = generate_request()
     resultText = ''
@@ -259,12 +104,12 @@ def getVoice2Text():
                   % (response.resultCd, response.recognizedText))
             break
 
-    print ("\n\n인식결과: %s \n\n\n" % (resultText).encode('utf-8'))
+    print ("\n\n인식결과: %s \n\n\n" % (resultText))
     return resultText
 
 def getText2VoiceStream(inText,inFileName):
 
-	channel = grpc.secure_channel('{}:{}'.format(HOST, PORT), getCredentials())
+	channel = grpc.secure_channel('{}:{}'.format(HOST, PORT), UA.getCredentials())
 	stub = gigagenieRPC_pb2_grpc.GigagenieStub(channel)
 
 	message = gigagenieRPC_pb2.reqText()
@@ -282,6 +127,8 @@ def getText2VoiceStream(inText,inFileName):
 	return response.resOptions.resultCd
 
 def main():
+	output_file = "testtts.wav"
+	
 	test_return=test()
 	
 	if(int(test_return)==200):
@@ -289,7 +136,6 @@ def main():
 		print("text : %s"%text)
 
 	if(text):
-		output_file = "testtts.wav"
 		getText2VoiceStream("안녕하세요. 반갑습니다.",output_file)
 		play_file(output_file)
 
