46489bdce271e1a8ea9f33cbd155c7c6093636ac
AtaraOvO <acqc870@gmall.com>
2019-04-11 10:37:40 +0900

    chatbot_module class design

--- narsha/chatbot_module.py
+++ narsha/chatbot_module.py
@@ -1,4 +1,27 @@
 
-class question:
-    pass 
+class question_db:
+    database = []
+    def set_create(): # create new same-meaning set 
+        pass
+    def set_find(): # is there the keyword in the set?
+        pass
+    def set_delete(): # delete the set that the keyword is existing
+        pass
+    def keyword_add(): # add the keyword in the set 
+        pass    
+    def keyword_delete(): # delete the keyword in the set
+        pass
+    def key_get(): # return the key if the keyword is in the database
+        pass
+   
+
+
+class answer_db:
+    database = []
+    def increase_weight():
+        pass
+    def decrease_weight():
+        pass
+
+
 # I will fill this module file
