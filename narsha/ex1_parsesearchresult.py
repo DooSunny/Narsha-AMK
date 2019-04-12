410581c3fdad63222098baf4dc4b6888d3e94ee3
AtaraOvO <acqc870@gmall.com>
2019-04-12 20:38:12 +0900

    add exception process

--- narsha/ex1_parsesearchresult.py
+++ narsha/ex1_parsesearchresult.py
@@ -5,25 +5,19 @@ import urllib.parse
 from bs4 import BeautifulSoup
 # import re
 
-def isResultExist(soup):
-    return True
-def wikipediaparser(soup):
-    find_results = soup.find("div",{"class":"mw-parser-output"})
-    if isResultExist(soup) == True:
-        return None
-    for i, data in enumerate(find_results):
-        if i == 0:
-            if str(data)[1] == 'p':
+def wikipediaparser(soup): # func about parse wikipedia
+    find_results = soup.find("div",{"class":"mw-parser-output"}) # find information div
+    for i, data in enumerate(find_results): 
+        if i == 0: # detect wrong data
+            if str(data)[1] == 'p': # is there wrong data?
                pass
-            else:
+            else: # if do this line, it is right data
                  result = soup.select(".mw-parser-output p")[0].text
                  return result
-        else: 
+        else: # where is right data...
             if i%2 == 0 and str(data)[1] == 'p':
                 result = data.text
                 return result
-    #select_results = soup.select(".mw-parser-output")
-    #text = select_results[0].text
     return None
 
 def main():
@@ -31,14 +25,17 @@ def main():
     keyword = "대구소프트웨어고등학교" # it will be changed to user-text
     keyword = urllib.parse.quote(keyword)
     url = url + keyword    
-    response = urllib.request.urlopen(url)
-    html = response.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    desc = wikipediaparser(soup)
-    if desc == None:
-        print("WOW!") # exception code
-    else:
-        pass # giga-genie says something 
+    try:
+        response = urllib.request.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        desc = wikipediaparser(soup)
+        if desc == None:
+            print("Something Wrong...") # exception code
+        else:
+            pass # giga-genie says something 
+    except:
+        print("No search Result") # giga-genie says something
 
 
 if __name__ == '__main__':
