d813b1049646a48fa7f567a6ecff3081524c9656
DooSunnny <0308engo@gmail.com>
2019-04-13 08:45:14 +0900

    역할 분리

--- narsha/ex1_Clock.py
+++ narsha/ex1_Clock.py
@@ -1,4 +1,5 @@
 import datetime
+import ex1_call_stt_tts
 # what time is it?
 def Clock_hour(): # 시간 [시]
 	time=["한","두","세","네","다섯","여섯","일곱","여덟","아홉","열","열한","열두"] # 만약 안해줄시 시간을 물을때 "한시"가 아니라 "일시"라고 대답한다
--- narsha/ex1_call_stt_tts.py
+++ narsha/ex1_call_stt_tts.py
@@ -150,10 +150,12 @@ def main():
 			getText2VoiceStream("안녕하세요. 반갑습니다.", output_file)
 		elif("이름" in text):
 			getText2VoiceStream("제 이름은 기가지니입니다.", output_file)
+
 		elif(("몇시" in text) or ("시간" in text) ):
 			hour=ex1_Clock.Clock_hour()
 			minute=ex1_Clock.Clock_minute()
 			getText2VoiceStream("지금은 "+hour+"시, "+str(minute)+"분 입니다.", output_file)
+			
 		else:
 			getText2VoiceStream("알아들을 수 가 없습니다. 다시한번 말씀 해주세요.", output_file)
 
