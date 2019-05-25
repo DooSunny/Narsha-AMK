bd6ef8abd1237f6d7cf4c2a47cf518a183a1a66e
DooSunny <0308engo@gmail,com>
2019-05-25 11:02:11 +0900

    2 차수 정

--- narsha/medicinemain.py
+++ narsha/medicinemain.py
@@ -1,11 +1,13 @@
-import medicine from medicine
+from medicine import medicine
 import call_stt
 import threading
 import re
 import datetime
 
 def main(med):
-	t1 = threading.Thread(target = calling,args=(med))
+	t1 = threading.Thread(target = calling(med))
+	t1.daemon=True
+	t1.start()
 	output_file = "testtts.wav"
 	i=0
 	while True :
@@ -31,33 +33,35 @@ def main(med):
 def calling(med):
 	while True :
 		text=call_stt.Call()
-		if (아침 in text):
+		if ("아침" in text):
+			numbers = re.findall("\d+",text)
+			print(type(numbers))
+			print(type(text))
+			print(type("오후"in text))
+			if(("오후" in text) or (int(numbers[0])> 12)):
+				med.setbreakfirst(med,numbers[0],1)
+			else:
+				med.setbreakfirst(med,numbers[0],0)
+			if("분" in text):
+				med.setbreakfirstminute(med,numbers[1])
+		if("점심" in text):
 			numbers = re.findall("\d+",text)
 			print(numbers)
-			if((오후 in text) or (numbers[0] > 12)):
-				med.setbreakfirst(med.medicine,numbers[0],1)
+			if(("오후" in text) or (numbers[0] > 12)):
+				med.setlaunch(med,numbers[0],1)
 			else:
-				med.setbreakfirst(med.medicine,numbers[0],0)
-			if(분 in text):
-				med.setbreakfirstminute(med.medicine,numbers[1])
-		if(점심 in text):
+				med.setlaunch(med,numbers[0],0)
+			if("분" in text):
+				med.setlaunchminute(med,numbers[1])
+		if ("저녁" in text):
 			numbers = re.findall("\d+",text)
 			print(numbers)
-			if((오후 in text) or (numbers[0] > 12)):
-				med.setlaunch(med.medicine,numbers[0],1)
+			if(("오후" in text) or (numbers[0] > 12)):
+				med.setdinner(med,numbers[0],1)
 			else:
-				med.setlaunch(med.medicine,numbers[0],0)
-			if(분 in text):
-				med.setlaunchminute(med.medicine,numbers[1])
-		if (저녁 in text):
-			numbers = re.findall("\d+",text)
-			print(numbers)
-			if((오후 in text) or (numbers[0] > 12)):
-				med.setdinner(med.medicine,numbers[0],1)
-			else:
-				med.setdinner(med.medicine,numbers[0],0)
-			if(분 in text):
-				med.setdinnerminute(med.medicine,numbers[1])
+				med.setdinner(med,numbers[0],0)
+			if("분" in text):
+				med.setdinnerminute(med,numbers[1])
 		else:
 			call_stt.getText2VoiceStream("알아들을 수 가 없습니다. 다시한번 말씀 해주세요.", output_file)
 
